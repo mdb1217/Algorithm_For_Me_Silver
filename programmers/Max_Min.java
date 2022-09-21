722cad6f2058081348bd612a9727a4a94eb9c0b7
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2022-09-21 23:13:57 +0900

    PS_LV2_최댓값과 최솟값

---
+++ programmers/Max_Min.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String s) {
+        StringTokenizer st = new StringTokenizer(s);
+        String answer = "";
+        int temp;
+        int min, max;
+
+        min = Integer.parseInt(st.nextToken());
+        max = min;
+        while(st.hasMoreTokens()) {
+            temp = Integer.parseInt(st.nextToken());
+            if(temp - min < 0) {
+                min = temp;
+            }
+            if(temp - max > 0) {
+                max = temp;
+            }
+        }
+
+        answer = min + " " + max;
+
+        return answer;
+    }
+}
